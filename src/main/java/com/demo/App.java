7048d158667f1eee026ee0d74a039fd025aa21e8
anujmehta-hub <anuj.mehta@terralogic.com>
2026-03-05 14:12:34 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -3,7 +3,7 @@ package com.demo;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI VM!");
+        System.out.println("Hello Anuj Mehta from Jenkins CI VM!");
     }
 
     // This method can be used by your tests
